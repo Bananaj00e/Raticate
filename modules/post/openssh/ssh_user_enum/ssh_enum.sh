71e1f1692b57db1e36efde5c7d59b533ee568dd3
Bananaj00e <58997090+Bananaj00e@users.noreply.github.com>
2020-01-19 14:35:42 -0500

    Delete ssh_enum.sh

--- modules/post/openssh/ssh_user_enum/ssh_enum.sh
+++
@@ -1,5 +0,0 @@
-
-read -p "Enter The Target Host : " host
-read -p "Enter Your Username Wordlist Path : " users
-clear
-python3 ~/../usr/share/Pikachu/modules/post/openssh/ssh_user_enum/ssh_enum.py $host -U $users
